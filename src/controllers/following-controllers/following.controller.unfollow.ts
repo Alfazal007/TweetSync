9fb3daec5f66ad41ff07fe5ac60be44e0a5d48dd
Alfazal007 <alfazal7777@gmail.com>
2024-03-13 06:04:16 +0530

    feat: added follow and unfollow users

--- src/controllers/following-controllers/following.controller.unfollow.ts
+++ src/controllers/following-controllers/following.controller.unfollow.ts
@@ -1,9 +1,39 @@
 import { Request, Response } from "express";
 import { asyncHandler } from "../../utils/AsyncHandler";
+import { ApiError } from "../../utils/ApiError";
+import { prisma } from "../../utils/prisma";
+import { ApiResponse } from "../../utils/ApiResponse";
 
 const unfollow = asyncHandler(async (req: Request, res: Response) => {
     const userRequester = req.user.id;
-    // check if not following already
-    // if following then unfollow -- delete the relationship from the database
+    const { followingUser } = req.body;
+    let followRelation;
+    try {
+        followRelation = await prisma.following.findFirst({
+            where: {
+                AND: [
+                    { followersId: { equals: userRequester } },
+                    { followingId: { equals: followingUser } },
+                ],
+            },
+        });
+    } catch (error) {
+        return res
+            .status(404)
+            .json(new ApiError(404, "You dont follow this user", []));
+    }
+    if (!followRelation) {
+        return res
+            .status(404)
+            .json(new ApiError(404, "You dont follow this user", []));
+    }
+    await prisma.following.delete({
+        where: {
+            id: followRelation.id,
+        },
+    });
+    return res
+        .status(200)
+        .json(new ApiResponse(200, "Unfollowed the user", {}));
 });
 export { unfollow };
